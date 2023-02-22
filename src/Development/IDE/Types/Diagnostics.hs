2b0d34b269fa7b1db656a9484d79277e7cea6c34
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2023-02-22 12:58:27 +0000

    Split message into own chunk, don't remove formatting

--- src/Development/IDE/Types/Diagnostics.hs
+++ src/Development/IDE/Types/Diagnostics.hs
@@ -69,15 +69,11 @@ prettyRange :: Range -> Doc Terminal.AnsiStyle
 prettyRange Range{..} = f _start <> "-" <> f _end
     where f Position{..} = pretty (show $ _line+1) <> colon <> pretty (show $ _character+1)
 
-stringParagraphs :: T.Text -> Doc a
-stringParagraphs = vcat . map (fillSep . map pretty . T.words) . T.lines
-
 showDiagnostics :: [FileDiagnostic] -> T.Text
 showDiagnostics = srenderPlain . prettyDiagnostics
 
 showDiagnosticsColored :: [FileDiagnostic] -> T.Text
 showDiagnosticsColored = srenderColored . prettyDiagnostics
-
 
 prettyDiagnostics :: [FileDiagnostic] -> Doc Terminal.AnsiStyle
 prettyDiagnostics = vcat . map prettyDiagnostic
@@ -90,13 +86,13 @@ prettyDiagnostic (fp, sh, LSP.Diagnostic{..}) =
         , slabel_ "Range:   " $ prettyRange _range
         , slabel_ "Source:  " $ pretty _source
         , slabel_ "Severity:" $ pretty $ show sev
-        , slabel_ "Message: "
-            $ case sev of
+        , pretty ("Message: " :: String)
+        , case sev of
               LSP.DsError -> annotate $ color Red
               LSP.DsWarning -> annotate $ color Yellow
               LSP.DsInfo -> annotate $ color Blue
               LSP.DsHint -> annotate $ color Magenta
-            $ stringParagraphs _message
+            $ pretty _message
         ]
     where
         sev = fromMaybe LSP.DsError _severity
